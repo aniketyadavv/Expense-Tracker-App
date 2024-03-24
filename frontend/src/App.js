d3a7570d21ceb770f6672705888da7eaabaa7ea8
Aniket Yadav <aniket912yadav@gmail.com>
2024-03-25 00:38:42 +0530

    Utils + Orb added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,30 @@
+import React, { useState, useMemo } from "react";
 import styled from "styled-components";
 import bg from "./img/bg.png"
 import { MainLayout } from "./styles/Layouts";
 import Orb from "./Components/Orb/Orb";
+import Navigation from "./Components/Navigation/Navigation";
 
 function App() {
+  const [active, setActive] = useState(1)
+
+  const displayData = () => {
+    
+  }
+
+  const orbMemo = useMemo(() => {
+    return <Orb />
+  }, [])
+
 
   return (
     <AppStyled bg={bg} className="App">
-      <Orb/>
+      {orbMemo}
       <MainLayout>
-        
+        <Navigation active={active} setActive={setActive} />
+        <main>
+          {displayData()}
+        </main>
       </MainLayout>
     </AppStyled>
   );
@@ -18,6 +33,17 @@ function App() {
 const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${props => props.bg});
+  position: relative;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
 `;
 
 
--- frontend/src/Components/Orb/Orb.js
+++ frontend/src/Components/Orb/Orb.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
+import { useWindowSize } from '../../utils/useWindowSize';
 
 function Orb() {
 
+    const { width, height } = useWindowSize()
+
+    console.log(width, height)
+
     const moveOrb = keyframes`
-     0%{
+        0%{
             transform: translate(0, 0);
         }
         50%{
-            transform: translate(400px, 500px);
+            transform: translate(${width}px, ${height / 2}px);
         }
         100%{
             transform: translate(0, 0);
@@ -23,16 +28,12 @@ function Orb() {
         margin-left: -37vh;
         margin-top: -37vh;
         background: linear-gradient(180deg, #F56692 0%, #F2994A 100%);
-        filter: blur(100px);
-        animation: ${moveOrb} 1s alternate linear infinite;
-
+        filter: blur(400px);
+        animation: ${moveOrb} 15s alternate linear infinite;
     `;
 
-
     return (
-        <OrbStyled>
-
-        </OrbStyled>
+        <OrbStyled></OrbStyled>
     )
 }
 

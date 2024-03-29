eace18c28355e8e02fdbde8db1528837cc2fcb9d
Aniket Yadav <aniket912yadav@gmail.com>
2024-03-29 23:58:32 +0530

    Income + Expense Form

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -4,10 +4,10 @@ const { addIncome, getIncomes, deleteIncome } = require('../controllers/income')
 const router = require('express').Router();
 
 router.post('/add-income', addIncome)
-    .get('/get-income', getIncomes)
+    .get('/get-incomes', getIncomes)
     .delete('/delete-income/:id', deleteIncome)
     .post('/add-expense', addExpense)
-    .get('/get-expense', getExpense)
+    .get('/get-expenses', getExpense)
     .delete('/delete-expense/:id', deleteExpense)
 
 module.exports = router

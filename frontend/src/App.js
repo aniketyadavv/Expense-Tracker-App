502c092ea69f3bbb00078e6146b70c79d81ef297
Aniket Yadav <aniket912yadav@gmail.com>
2024-03-29 17:08:53 +0530

    Added Form to get Income

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,31 @@ import bg from "./img/bg.png"
 import { MainLayout } from "./styles/Layouts";
 import Orb from "./Components/Orb/Orb";
 import Navigation from "./Components/Navigation/Navigation";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Income from "./Components/Income/Income";
+import Expenses from "./Components/Expenses/Expenses";
+import { useGlobalContext } from "./context/globalContext";
+
 
 function App() {
   const [active, setActive] = useState(1)
 
+  const global = useGlobalContext()
+  console.log(global)
+
   const displayData = () => {
-    
+    switch (active) {
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Income />
+      case 4:
+        return <Expenses />
+      default:
+        return <Dashboard />
+    }
   }
 
   const orbMemo = useMemo(() => {
